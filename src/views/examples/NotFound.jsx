3228c8f09745a7f668aa9c11605483210e61dd15
Erick Costa <erickcosta2509@gmail.com>
2021-09-09 16:33:11 -0300

    configuração página 404

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Home = props => (
+    <div className='Home'>
+        <h1>404</h1>
+        <h3>Ops... Página não encontrada!</h3>
+    </div>
+)
+
+export default Home
